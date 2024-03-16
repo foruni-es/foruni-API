bdb24d79de97344358751755da6d451165a76cfb
gobuperez <gobuperez@gmail.com>
2024-03-16 22:21:11 +0100

    Configuración de CORS

--- constants.js
+++ constants.js
@@ -1,6 +1,9 @@
 const DEVELOPMENT = 'development';
 const PRODUCTION = 'production';
 
+const DEVELOPMENT_DOMAIN = 'http://localhost:3000';
+const PRODUCTION_DOMAIN = 'https://foruni.es';
+
 const ERROR_LOGIN = 'Usuario o contraseña incorrectos.';
 const ERROR_400 = 'El formato de la petición no es correcto.';
 const ERROR_401 = 'Por favor, inicia sesión en tu cuenta.';
@@ -10,6 +13,8 @@ const ERROR_500 = 'Se ha producido un error en la aplicación.';
 module.exports = {
     DEVELOPMENT,
     PRODUCTION,
+    DEVELOPMENT_DOMAIN,
+    PRODUCTION_DOMAIN,
     ERROR_LOGIN,
     ERROR_400,
     ERROR_401,
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const cors = require('cors');
+const corsConfig = require('./cors/corsConfig');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 
@@ -12,11 +12,7 @@ const userRoutes = require('./routes/userRoutes');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser()); 
-/* app.use(cors({
-    origin: "http://localhost:3000",
-    credentials: true
-})); */
-app.use(cors());
+app.use(corsConfig);
 app.use(helmet());
 
 app.use('/universities', universityRoutes);
