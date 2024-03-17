f4438fa152dd0401e0f69290f77ebdd7c22e90dd
gobuperez <gobuperez@gmail.com>
2024-03-17 12:37:21 +0100

    Corrigiendo errores despliegue

--- database/db.js
+++ database/db.js
@@ -2,14 +2,21 @@ const { PRODUCTION } = require('../constants');
 
 const pgp = require('pg-promise')();
 
-const connectionParameters = process.env.NODE_ENV === PRODUCTION ? process.env.DATABASE_URL :
-{
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
-} 
+const connectionParameters = process.env.NODE_ENV === PRODUCTION ? 
+    {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+    :
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_DATABASE,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD
+    } 
 
 const db = pgp(connectionParameters);
 
